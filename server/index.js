af0ec82f82ee83ae7753de803679b7071d20949c
techswarn <tech.swarns@gmail.com>
2022-09-27 16:18:07 +0530

    code update

--- server/index.js
+++ server/index.js
@@ -1,19 +1,53 @@
-const http = require('http').createServer()
-const dotenv = require('dotenv')
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 
-dotenv.config({path: './config.env'})
-
-const io = require('socket.io')(http, {
+const io = require('socket.io')(server, {
     cors: {origin: '*'}
 })
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
 
-    socket.on('message', (message) =>{
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+    socket.on('message', (message) => {
         console.log(message);
         io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
     });
-})
+});
 
-http.listen(8080, () => console.log('listening on http://localhost:8080') );
+server.listen(8080, () => {
+  console.log('listening on *:8080');
+});
+
+// const http = require('http').createServer()
+// const express = require('express');
+// const app = express();
+// const httpnew = require('http');
+// const server = httpnew.createServer(app);
+// const { Server } = require("socket.io");
+
+// const port = 8080
+// const dotenv = require('dotenv')
+
+// dotenv.config({path: './config.env'})
+
+// const io = require('socket.io')(http, {
+//     cors: {origin: '*'}
+// })
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+
+//     socket.on('message', (message) =>{
+//         console.log(message);
+//         io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
+//     });
+// })
+
+// http.listen(8080, () => console.log('listening on http://localhost:8080') );
