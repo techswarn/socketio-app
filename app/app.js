5efee5e5848771dc2fa4983c6f32523d475e22d5
techswarn <tech.swarns@gmail.com>
2022-11-14 13:06:10 +0530

    added env for production and dev

---
+++ app/app.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv')
+
+dotenv.config({path: './config.env'})
+
+let socket;
+if(process.env.NODE_ENV === 'development') {
+    socket = io('ws://localhost:8080')
+} else if (process.env.NODE_ENV === 'production') {
+    socket = io(`ws://${process.env.APPURL}`)
+}
+
+socket.on('message', text => {
+    const el = document.createElement('li');
+    el.innerHTML = text;
+    document.querySelector('ul').appendChild(el)
+})
+
+document.querySelector('button').onclick = () => {
+
+    const text = document.querySelector('input').value;
+    socket.emit('message', text)
+    
+}
